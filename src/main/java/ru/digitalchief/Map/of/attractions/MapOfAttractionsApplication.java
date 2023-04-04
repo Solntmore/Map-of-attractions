2df5166ef9fa0be65fe4a1a9637345f31c9c4cc1
Solntsev Maksim <solntsev1099@gmail.com>
2023-04-04 10:01:59 +0300

    Initial commit

---
+++ src/main/java/ru/digitalchief/Map/of/attractions/MapOfAttractionsApplication.java
@@ -0,0 +1,13 @@
+package ru.digitalchief.Map.of.attractions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapOfAttractionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapOfAttractionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/digitalchief/Map/of/attractions/MapOfAttractionsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.digitalchief.Map.of.attractions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapOfAttractionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
