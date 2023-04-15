6bc455b5dd6b9590cfb84daffc944ea3dbfdda05
Solntsev Maksim <solntsev1099@gmail.com>
2023-04-15 14:02:28 +0300

    add: добавлено кеширование на redis add: добавлен класс RedisConfig для настройки подключения add: в response классы добавлены аннотации @Buidlder и @Jacksonized, а также @JsonProperty над полями для корректной сериализации и десериализации. add: в application.properties добавлены параметры подключения к redis add: в docker-compose.yml добавлен контейнер cache, отвечающий за кеширование

--- src/main/java/ru/digitalchief/Map/of/attractions/dto/ResponseAttractionDto.java
+++ src/main/java/ru/digitalchief/Map/of/attractions/dto/ResponseAttractionDto.java
@@ -1,6 +1,9 @@
 package ru.digitalchief.Map.of.attractions.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 import ru.digitalchief.Map.of.attractions.model.Status;
 
 import java.io.Serializable;
@@ -9,15 +12,22 @@ import java.io.Serializable;
  * A DTO for the {@link ru.digitalchief.Map.of.attractions.model.Attraction} entity
  */
 @Data
+@Builder
+@Jacksonized
 public class ResponseAttractionDto implements Serializable {
 
+    @JsonProperty("id")
     private final Long id;
 
+    @JsonProperty("name")
     private final String name;
 
+    @JsonProperty("city")
     private final ShortCityDto city;
 
+    @JsonProperty("status")
     private final Status status;
 
+    @JsonProperty("website")
     private final String website;
 }
